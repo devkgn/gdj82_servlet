79b87dcfa16647f5d37365e144192e7b386374c6
devkgn <devkgn13@gmail.com>
2024-07-04 12:43:29 +0900

    [김가남] lec03_filter

---
+++ lec03_filter/src/main/java/com/gn/common/FilterTest.java
@@ -0,0 +1,36 @@
+package com.gn.common;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+
+@WebFilter(servletNames = "filterTestServlet")
+public class FilterTest extends HttpFilter implements Filter {
+       
+
+    public FilterTest() {
+        super();
+    }
+
+	public void destroy() {
+		// 필터 종료
+	}	
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// 실제 클라이언트의 요청과 응답을 가로채는 메소드
+		System.out.println("필터 동작 시점 확인");
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		// 필터 초기화 단계에서 호출
+	}
+
+}
