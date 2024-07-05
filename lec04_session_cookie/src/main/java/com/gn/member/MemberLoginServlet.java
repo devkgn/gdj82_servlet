7d56d87da704dd6886a966f769ceca113c435d2e
devkgn <devkgn13@gmail.com>
2024-07-05 12:42:37 +0900

    [김가남] JDBC 코드 작성

--- lec04_session_cookie/src/main/java/com/gn/member/MemberLoginServlet.java
+++ lec04_session_cookie/src/main/java/com/gn/member/MemberLoginServlet.java
@@ -2,14 +2,17 @@ package com.gn.member;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet("/login")
@@ -29,7 +32,51 @@ public class MemberLoginServlet extends HttpServlet {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
+		Member m = null;
 		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://127.0.0.1:3306/jdbc_test";
+			String user = "scott";
+			String pw = "tiger";
+			conn = DriverManager.getConnection(url, user, pw);
+			String sql = "SELECT * FROM member WHERE member_id = ? AND member_pwd = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, memberId);
+			pstmt.setString(2, memberPw);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				m = new Member(rs.getInt("member_no")
+						,rs.getString("member_id")
+						,rs.getString("member_pwd")
+						,rs.getString("member_name")
+						,rs.getString("member_email")
+						,rs.getString("member_phone")
+						,rs.getString("member_gender")
+						,rs.getTimestamp("reg_date").toLocalDateTime()
+						,rs.getTimestamp("mod_date").toLocalDateTime());
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				rs.close();
+				pstmt.close();
+				conn.close();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		HttpSession session = request.getSession(true);
+		if(session.isNew() || session.getAttribute("member") == null) {
+			session.setAttribute("member", m);
+			session.setMaxInactiveInterval(10);
+		}
+		
+		
+		response.sendRedirect("/");
 	}
 
 
