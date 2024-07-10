9266dbf63e8af78394c9f8ab7250e0d6e03ca140
devkgn <devkgn13@gmail.com>
2024-07-10 15:29:15 +0900

    [김가남] 게시판 목록

---
+++ lec06_mvc/src/main/java/com/gn/board/controller/BoardListServlet.java
@@ -0,0 +1,42 @@
+package com.gn.board.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.gn.board.service.BoardService;
+import com.gn.board.vo.Board;
+
+@WebServlet("/board/list")
+public class BoardListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public BoardListServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String title = request.getParameter("board_title");
+		
+		Board option = new Board();
+		option.setBoard_title(title);
+		
+		List<Board> list = new BoardService().selectBoardList(option);
+		
+		1. /views/board/list.jsp
+		2. css 추가
+		3. 화면 이동 작성
+		4. 화면 이동할때 속성값을 전달
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
--- lec06_mvc/src/main/java/com/gn/board/service/BoardService.java
+++ lec06_mvc/src/main/java/com/gn/board/service/BoardService.java
@@ -4,6 +4,7 @@ import static com.gn.common.sql.JDBCTemplate.close;
 import static com.gn.common.sql.JDBCTemplate.getConnection;
 
 import java.sql.Connection;
+import java.util.List;
 
 import com.gn.board.dao.BoardDao;
 import com.gn.board.vo.Board;
@@ -16,4 +17,12 @@ public class BoardService {
 		close(conn);
 		return result;
 	}
+	
+	public List<Board> selectBoardList(Board option){
+		Connection conn = getConnection();
+		List<Board> list = new BoardDao().selectBoardList(option,conn);
+		close(conn);
+		return list;
+	}
+	
 }
