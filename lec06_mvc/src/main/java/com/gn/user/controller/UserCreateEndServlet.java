68df68655b043d9fa4229cb7d8a18060250dbb65
devkgn <devkgn13@gmail.com>
2024-07-10 09:44:42 +0900

    [김가남] 로그인 이동 href 추가

--- lec06_mvc/src/main/java/com/gn/user/controller/UserCreateEndServlet.java
+++ lec06_mvc/src/main/java/com/gn/user/controller/UserCreateEndServlet.java
@@ -35,15 +35,15 @@ public class UserCreateEndServlet extends HttpServlet {
 
 		int result = new UserService().createUser(u);
 		
-//		RequestDispatcher view 
-//		= request.getRequestDispatcher("/views/user/create_fail.jsp");
-//		if(result > 0) {
-//			view = request.getRequestDispatcher("/views/user/create_success.jsp");
-//		} 
-//		view.forward(request,response);
+		RequestDispatcher view 
+		= request.getRequestDispatcher("/views/user/create_fail.jsp");
+		if(result > 0) {
+			view = request.getRequestDispatcher("/views/user/create_success.jsp");
+		} 
+		view.forward(request,response);
 		
-		if(result > 0 ) System.out.println("성공");
-		else System.out.println("실패");
+//		if(result > 0 ) System.out.println("성공");
+//		else System.out.println("실패");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
