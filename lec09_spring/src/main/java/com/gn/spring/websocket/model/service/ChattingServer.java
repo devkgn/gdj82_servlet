923c5c342044929fdd8f938927d0f1cce1440c98
devkgn <devkgn13@gmail.com>
2024-08-06 15:24:51 +0900

    [김가남] 응답 메시지 설정(Server)

--- lec09_spring/src/main/java/com/gn/spring/websocket/model/service/ChattingServer.java
+++ lec09_spring/src/main/java/com/gn/spring/websocket/model/service/ChattingServer.java
@@ -1,5 +1,10 @@
 package com.gn.spring.websocket.model.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -9,10 +14,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gn.spring.websocket.model.vo.SendMessage;
 
 public class ChattingServer extends TextWebSocketHandler{
-
+	
+	private List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
+	
 	// 클라이언트가 연결되었을 때 설정
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		sessionList.add(session);
 	}
 
 	// 클라이언트가 웹소켓 서버로 메시지를 전송했을 때 설정
@@ -24,12 +32,22 @@ public class ChattingServer extends TextWebSocketHandler{
 		ObjectMapper objectMapper = new ObjectMapper();
 		// JSON String 형태 데이터 -> 우리가 만든 클래스 형태로 변환
 		SendMessage msg = objectMapper.readValue(payload, SendMessage.class);
-		System.out.println(msg);
+		
+		Map<String,String> resultMap = new HashMap<String,String>();
+		resultMap.put("res_code", "404");
+		resultMap.put("res_msg", "채팅 전송중 오류가 발생하였습니다.");
+		
+		TextMessage tm = new TextMessage(objectMapper.writeValueAsString(resultMap));
+		
+		for(WebSocketSession temp : sessionList) {
+			temp.sendMessage(tm);
+		}
 	}
 
 	// 클라이언트가 연결을 끊었을 때 설정
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		sessionList.remove(session);
 	}
 	
 }
