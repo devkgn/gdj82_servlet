70d2e343a84c3e678956de50ada48cf8d05dfaab
devkgn <devkgn13@gmail.com>
2024-08-06 14:51:12 +0900

    [김가남] websocket 기본 설정

---
+++ lec09_spring/src/main/java/com/gn/spring/websocket/controller/ChattingController.java
@@ -0,0 +1,14 @@
+package com.gn.spring.websocket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChattingController {
+	
+	@GetMapping("/chattingPage")
+	public String chattingPage() {
+		return "chat/list";
+	}
+	
+}
