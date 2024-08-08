3681cceff196301ff698dfd385b4fe1853b6a6d0
devkgn <devkgn13@gmail.com>
2024-08-08 12:29:18 +0900

    [김가남] 목록 조회

---
+++ lec10_boot/src/main/java/com/gn/spring/board/service/BoardService.java
@@ -0,0 +1,34 @@
+package com.gn.spring.board.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gn.spring.board.domain.Board;
+import com.gn.spring.board.domain.BoardDto;
+import com.gn.spring.board.repository.BoardRepository;
+
+@Service
+public class BoardService {
+	
+	private final BoardRepository boardRepository;
+	
+	@Autowired
+	public BoardService(BoardRepository boardRepository) {
+		this.boardRepository = boardRepository;
+	}
+
+	public List<BoardDto> selectBoardList(){
+		List<Board> boardList = boardRepository.findAll();
+		
+		List<BoardDto> boardDtoList = new ArrayList<BoardDto>();
+		// Board(Entity) 를 BoardDto로 바꿔주기
+		for(Board board : boardList) {
+			BoardDto boardDto = new BoardDto().toDto(board);
+			boardDtoList.add(boardDto);
+		}
+		return boardDtoList;
+	}
+}
