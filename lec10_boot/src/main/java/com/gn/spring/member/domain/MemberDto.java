460c858ccfd34ff2458552e362aa641c2c95890c
devkgn <devkgn13@gmail.com>
2024-08-12 11:53:19 +0900

    [김가남] JPA 연관관계 적용

---
+++ lec10_boot/src/main/java/com/gn/spring/member/domain/MemberDto.java
@@ -0,0 +1,45 @@
+package com.gn.spring.member.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class MemberDto {
+	
+	private Long mem_no;
+	private String mem_id;
+	private String mem_pw;
+	private String mem_name;
+	private String mem_auth;
+	
+	public Member toEntity() {
+		return Member.builder()
+				.memNo(mem_no)
+				.memId(mem_id)
+				.memPW(mem_pw)
+				.memName(mem_name)
+				.memAuth(mem_auth)
+				.build();
+	}
+	
+	public MemberDto toDto(Member member){
+		return MemberDto.builder()
+				.mem_no(member.getMemNo())
+				.mem_id(member.getMemId())
+				.mem_pw(member.getMemPW())
+				.mem_name(member.getMemName())
+				.mem_auth(member.getMemAuth())
+				.build();
+	}
+	
+
+}
