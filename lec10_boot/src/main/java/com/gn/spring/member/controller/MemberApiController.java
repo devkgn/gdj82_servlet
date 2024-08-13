b8949d663271c7299578009da7650faaa8d895dd
devkgn <devkgn13@gmail.com>
2024-08-13 10:50:09 +0900

    [김가남] 회원가입 기능 메소드 선언부 구성

---
+++ lec10_boot/src/main/java/com/gn/spring/member/controller/MemberApiController.java
@@ -0,0 +1,13 @@
+package com.gn.spring.member.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberApiController {
+
+	// 1. 메소드명 : joinMember
+	// 2. 매개변수 : json형태 데이터 MemberDto
+	// 3. 리턴 : json형태 데이터 Map<String,String>
+	// 4. 통신 : /join , POST
+	
+}
