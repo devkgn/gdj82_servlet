e99ded732ecad217f9f015a45e617eb4c687a704
devkgn <devkgn13@gmail.com>
2024-08-13 11:36:23 +0900

    [김가남] 회원가입 기능 구현

--- lec10_boot/src/main/java/com/gn/spring/security/config/WebSecurityConfig.java
+++ lec10_boot/src/main/java/com/gn/spring/security/config/WebSecurityConfig.java
@@ -1,14 +1,13 @@
 package com.gn.spring.security.config;
 
-import java.net.http.HttpRequest;
-
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -42,5 +41,10 @@ public class WebSecurityConfig {
 				.requestMatchers(
 						PathRequest.toStaticResources().atCommonLocations()
 				));
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
