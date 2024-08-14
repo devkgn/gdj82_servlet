fe4a849f0b6b82cd0d647aa9b6a7d3c7f0fdbc37
devkgn <devkgn13@gmail.com>
2024-08-14 11:11:38 +0900

    [김가남] 채팅 생성 가능 회원목록 조회

--- lec10_boot/src/main/java/com/gn/spring/member/repository/MemberRepository.java
+++ lec10_boot/src/main/java/com/gn/spring/member/repository/MemberRepository.java
@@ -13,6 +13,19 @@ public interface MemberRepository extends JpaRepository<Member,Long>{
 	
 	Member findBymemId(String mem_id);
 	
-	@Query(value="")
+	@Query(value="SELECT m "
+			+ "FROM Member m "
+			+ "WHERE m.memId != ?1 "
+			+ "AND m.memAuth != 'ADMIN' "
+			+ "AND(m.memId NOT IN (SELECT cr.fromId "
+			+ "FROM ChatRoom cr "
+			+ "WHERE cr.toId = ?1 )) "
+			+ "AND(m.memId NOT IN (SELECT cr.toId "
+			+ "FROM ChatRoom cr "
+			+ "WHERE cr.fromId = ?1 ))")
 	List<Member> findAllForChat(String mem_id);
 }
+
+
+
+
